53e029cf62b63a737bf16c007175054e2eb6e1bc
AudioTore <95601200+AudioTore@users.noreply.github.com>
2022-09-06 06:44:16 -0700

    Update Kernel.cs

--- src/Kernel/Kernel.cs
+++ src/Kernel/Kernel.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using Sys = Cosmos.System;
 using System.Threading;
-using Cosmos.HAL;
+using Sys = Cosmos.System;
 
 /* EchOS 2022 This is the base of the operating system
  * and where most of the Operating system spends its time.
@@ -11,242 +8,125 @@ using Cosmos.HAL;
  * modify it, Make it goofy ahh, Go crazy bro!
  * 
  * Also if you are planning to modify the code MAKE SURE that you DO have Cosmos AND Vmware Installed. Otherwise there will be build errors when compiling this.
+ * Go figure this out yourself as I will most likely not be providing any documentation on how to do this. If you have some experience in coding editing the source code should be fairly easy.
+ * as this used super basic code.
  */
 
-namespace EchOS_Dev
+namespace EchOS
 {
-    public class Kernel : Sys.Kernel
-    {
-        Cmdman cmd = new Cmdman(); // Command manager.
 
-        public bool flag;
-        protected override void BeforeRun()
-        {
-            flag = false;
-            Console.Clear(); // Clear all the 'pre' boot messages.
-                             // Animation frames for the 3D cube.
-           
-            
-            Console.WriteLine(@"                    #
-                     ######
-                 ####   #  ##
-              ####      #   ##
-           ####         #     ##
-        ###             #      ##
-      ##                #       ###
-      ###               #         ##
-      # ##              #           ##
-      #   ##            #          ###
-      #    ##           #       ###  #
-      #      ##         #    ###     #
-      #       ##        #####        #
-      #         ##    ###            #
-      #          #####  #            #
-      #            #    #            #
-      #            #    #            #
-      #            #    #            #
-      #            #    #            #
-      #            #    #            #
-      #            #    #            #
-      #            #  #####          #
-      #            ###    ##         #
-      #        #####        ##       #
-      #     ###    #         ##      #
-      #  ###       #           ##    #
-      ###          #            ##   #
-      ##           #              ## #
-        ##         #               ###
-         ###       #                ##
-           ##      #             ###
-            ##     #         ####
-              ##   #      ####
-               ##  #   ####
-                 ######
-                   #");
-            Console.Clear();
-            Console.WriteLine(@"  #
-              ######
-            ## #    ###
-           ##  #       ###
-         ##    #          ####
-        ##     #              ####
-      ##       #                 ##
-     ##        #                ###
-    #          #               ## #
-   ##          #             ##   #
-   # ####      #            ##    #
-   #     ####  #           #      #
-   #        ####         ##       #
-   #           #####   ##         #
-   #           #    ####          #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #          ####    #           #
-   #         ##   #####           #
-   #       ##         ####        #
-   #      #           #  ####     #
-   #    ##            #      #### #
-   #   ##             #          ##
-   # ##               #          #
-   ###                #        ##
-   ##                 #       ##
-    ####              #     ##
-        ####          #    ##
-            ###       #  ##
-               ###    # ##
-                  ######
-                      #");
-            Console.Clear();
-            Console.WriteLine(@"
-              ######
-            ## #    ###
-           ##  #       ###
-         ##    #          ####
-        ##     #              ####
-      ##       #                 ##
-     ##        #                ###
-    #          #               ## #
-   ##          #             ##   #
-   # ####      #            ##    #
-   #     ####  #           #      #
-   #        ####         ##       #
-   #           #####   ##         #
-   #           #    ####          #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #           #      #           #
-   #          ####    #           #
-   #         ##   #####           #
-   #       ##         ####        #
-   #      #           #  ####     #
-   #    ##            #      #### #
-   #   ##             #          ##
-   # ##               #          #
-   ###                #        ##
-   ##                 #       ##
-    ####              #     ##
-        ####          #    ##
-            ###       #  ##
-               ###    # ##
-                  ######
-                      #");
-            Console.Clear();
-            Console.Write(@"    #
-          ##############
-          #             ##########
-         ##                     ##
-         ##                     ##
-        ###                    ###
-        # #                    # #
-        # #                    # #
-       ## #                   ## #
-       #  #                   #  #
-      ##  #                   #  #
-      ##############         #   #
-      #   #         ##########   #
-      #   #                  #   #
-      #   #                  #   #
-      #   #                  #   #
-      #   #                  #   #
-      #   #                  #   #
-      #   #                  #   #
-      #   #                  #   #
-      #   #                  #   #
-      #   ##########         #   #
-      #   #         ##############
-      #  #                   #  ##
-      #  #                   #  #
-      # ##                   # ##
-      # #                    # #
-      # #                    # #
-      ###                    ###
-      ##                     ##
-      ##                     ##
-      ##########             #
-                ##############
-                             #");
+	public class Kernel : Sys.Kernel
+	{
 
-            // Give the OS A little more time to boot. (Resson for the Thread Sleeps.)
-            Console.Clear();
-            Thread.Sleep(1000);
-            Console.WriteLine("EchOS");
-            Thread.Sleep(2000);
-            Console.Clear();
-            Console.WriteLine("Booting.");
-            Thread.Sleep(1000);
-            Console.WriteLine("Booting..");
-            Thread.Sleep(1000);
-            Console.WriteLine("Booting...");
-            Thread.Sleep(1000);
-            Console.Clear();
-            Console.WriteLine("EchOS Has successfully booted!");
-            Thread.Sleep(2000);
-            Console.Clear();
+		protected override void BeforeRun()
+		{
 
-            Console.WriteLine("Welcome!");
-            Console.WriteLine("#########################################");
-            Console.WriteLine("               EchOS 2.0");
-            Console.WriteLine("#########################################");
-            Console.WriteLine("Type 'Help;' to get a list of commands.");
-        }
+			Thread.Sleep(2000);
+			Console.Clear();
 
-        protected override void Run() // While loop by default.
-        {
-            Console.Write("[0]: ");
-            string input_cmd = Console.ReadLine();
-
-            switch (input_cmd)
+            for (int index = 0; index < 5; index+=1)
             {
-                    case "Help;":
-                        Cmdman.help();
-                        break;
 
-                    case "System_Shutdown;":
-                        Cmdman.Shutdown();
-                        break;
+                Console.WriteLine("Booting.");
+                Thread.Sleep(1000);
 
-                    case "System_Reboot;":
-                        Cmdman.Reboot();
-                        break;
+				Console.WriteLine("Booting..");
+				Thread.Sleep(1000);
 
-                    case "Clear_Src;":
-                        Cmdman.Clear_Src();
-                        break;
+                Console.WriteLine("Booting...");
+                Thread.Sleep(1000);
 
-                    case "BackgroundColor_Change;":
-                        Cmdman.BackgroundColor_Change();
-                        break;
+                Console.Clear();
 
-                    case "Calculator;":
-                        Cmdman.Calculator();
-                        break;
-
-                    case "About;":
-                        Cmdman.About();
-                        break;
-
-
-                    default:
-                        Console.WriteLine("'" + input_cmd + "' Is not a vaild command.");
-                        Console.WriteLine("NOTE: Every command has a semicolumn at the end and a capital at the beginning.");
-                        break;
-
-
-                }
-                if (String.IsNullOrEmpty(input_cmd))
-                {
-                    Console.WriteLine("Command cannot be empty.");
-                }
             }
+
+            Console.Beep();
+			Console.WriteLine("EchOS Successfully booted!");
+			Thread.Sleep(2000);	
+
+			Console.Clear();
+
+            Console.WriteLine("===========================================");
+            Console.WriteLine("||########################################||");
+            Console.WriteLine("||               EchOS 3.0                ||");
+            Console.WriteLine("||########################################||");
+			Console.WriteLine("===========================================");
+			Console.WriteLine("Type 'Help();' to get a list of commands.");
+
+
         }
 
-   
+		protected override void Run() // While loop by default.
+		{
+
+			while (true)
+			{
+				
+				Console.Write(@"[0:\\]: ");
+				string input_cmd = Console.ReadLine();
+
+					switch (input_cmd)
+					{
+
+						case "Help();":
+							Cmdman.Help();
+							break;
+
+						case "System.Shutdown();":
+							Cmdman.Shutdown();
+							break;
+
+						case "System.Reboot();":
+							Cmdman.Reboot();
+							break;
+
+						case "Console.Clear();":
+							Cmdman.Clear_Src();
+							break;
+
+						case "BackgroundColor.Change();":
+							Cmdman.BackgroundColor_Change();
+							break;
+
+						case "Calculator();":
+							Cmdman.Calculator();
+							break;
+
+						case "System.About();":
+							Cmdman.About();
+							break;
+
+
+						default:
+							Console.WriteLine("'" + input_cmd + "' Is not a vaild command.");
+							Console.WriteLine("NOTE: Every command has a capital at the beginning and a '();' at the end.");
+							break;
+
+					}
+
+					// If input null.
+					if (string.ReferenceEquals(input_cmd, null) || input_cmd.Length == 0)
+					{
+
+						Console.WriteLine("Command cannot be empty or null.");
+
+					}
+				
+			}
+				
+		}
+				
+	}
+
 }
+
+
+
+
+		
+
+
+
+
+
 
