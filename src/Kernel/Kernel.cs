34a4d08a40a5fa6bd1e7ca767c07bee194eaa5c0
AudioTore <95601200+AudioTore@users.noreply.github.com>
2022-10-01 07:32:13 -0600

    Update Kernel.cs

--- src/Kernel/Kernel.cs
+++ src/Kernel/Kernel.cs
@@ -2,7 +2,9 @@ using System;
 using System.Threading;
 using Sys = Cosmos.System;
 
-/* EchOS 2022 This is the base of the operating system
+
+/* 
+ * EchOS 2022 This is the base of the operating system
  * and where most of the Operating system spends its time.
  * Note that you can do anything with this system. Republish it with or without credit,
  * modify it, Make it goofy ahh, Go crazy bro!
@@ -10,6 +12,7 @@ using Sys = Cosmos.System;
  * Also if you are planning to modify the code MAKE SURE that you DO have Cosmos AND Vmware Installed. Otherwise there will be build errors when compiling this.
  * Go figure this out yourself as I will most likely not be providing any documentation on how to do this. If you have some experience in coding editing the source code should be fairly easy.
  * as this used super basic code.
+ * 
  */
 
 namespace EchOS
@@ -18,11 +21,21 @@ namespace EchOS
 	public class Kernel : Sys.Kernel
 	{
 
+		Sys.FileSystem.CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
+
 		protected override void BeforeRun()
 		{
 
-			Thread.Sleep(2000);
+			Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+
 			Console.Clear();
+
+			Console.WriteLine("Kernel loaded..");
+			Thread.Sleep(1000);
+			Console.Clear();
+
+			
+
 
             for (int index = 0; index < 5; index+=1)
             {
@@ -40,19 +53,40 @@ namespace EchOS
 
             }
 
-            Console.Beep();
-			Console.WriteLine("EchOS Successfully booted!");
-			Thread.Sleep(2000);	
+			// Ascii Logo.
+			Console.WriteLine(@"
 
+					:~7Y5PPPP5YJ?!^.                          
+				:75GBBBBBBBGGGPP55J7^.				^7777777777~
+			:JG#BBB###BBBBBBBBBG5YY?~.				P@@@@@@@@@@5   
+			!G#BB##BBBBBBBBGGGGBBBG5JJ7:			#@@&5YYYYY5~   
+			7B#B##BBBBBBBBBBBBBGGGGG#P???:			@@@5           
+			~BBB##BBBB#BPY?77?YPBBGGGG#P??7.		7@@@J.:::::.    
+			YBBB#BBBB#GY!.    :!JBBPPGG#J??^		5@@@@@@@@@@Y    
+			5BGB#BBGB#P?.      :7P#GPGG#Y??~		B@@&BBBBBBB!    
+			JGGG#BBBB#GJ!.    .~JBBPGGG#J??^ :		@@@P            
+			^PGPB#BBGGBB5J7!!7J5BBPPGG#P??7.		7@@@?            
+			!PPPB#BGGGGBBBBBBBBGPPGG#P???:			5@@@7:::::^.     
+			~YP5PBBBGGGGGGGPPPGGBBGY??7:			#@@@@@@@@@@?     
+			.7YYY5PGBBBGGGBBBGG5J??7^:G     
+				.~7?JJJYYY55YYJJ??7!:                        
+					.:~!!777777!~^:.    
+
+
+			");
+
+			Console.Beep();
+			Thread.Sleep(2000);
 			Console.Clear();
 
             Console.WriteLine("===========================================");
             Console.WriteLine("||########################################||");
-            Console.WriteLine("||               EchOS 3.0                ||");
+            Console.WriteLine("||               EchOS 1.3                ||");
             Console.WriteLine("||########################################||");
 			Console.WriteLine("===========================================");
-			Console.WriteLine("Type 'Help();' to get a list of commands.");
-
+			Console.WriteLine("Type \"Help();\" to get a list of commands.");
+			Console.WriteLine("Or type \"Open.Tour();\" to take a tour to learn how to use EchOS.");
+			
 
         }
 
@@ -62,7 +96,7 @@ namespace EchOS
 			while (true)
 			{
 				
-				Console.Write(@"[0:\\]: ");
+				Console.Write(@"[0:\]: ");
 				string input_cmd = Console.ReadLine();
 
 					switch (input_cmd)
@@ -88,15 +122,27 @@ namespace EchOS
 							Cmdman.BackgroundColor_Change();
 							break;
 
-						case "Calculator();":
-							Cmdman.Calculator();
+						case "Open.Calculator();":
+							Calculator.Main(null);
 							break;
 
 						case "System.About();":
 							Cmdman.About();
 							break;
 
+						case "Open.Tour();":
+							Tour.Main(null);
+							break;
 
+						case "Open.TextPad();":
+							Textpad.textpad();
+							break;
+
+						case "Disk.GetInformation();":
+							Cmdman.DiskGetInformation();
+							break;
+
+						
 						default:
 							Console.WriteLine("'" + input_cmd + "' Is not a vaild command.");
 							Console.WriteLine("NOTE: Every command has a capital at the beginning and a '();' at the end.");
@@ -104,21 +150,22 @@ namespace EchOS
 
 					}
 
-					// If input null.
+					// If input has no characters.
 					if (string.ReferenceEquals(input_cmd, null) || input_cmd.Length == 0)
 					{
 
 						Console.WriteLine("Command cannot be empty or null.");
 
 					}
-				
+
 			}
-				
+
 		}
-				
+
 	}
 
 }
+
 
 
 
